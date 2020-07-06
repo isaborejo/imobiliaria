e79fe6dfbb9f7097722b0afc5433ac5b3eeb0991
isaborejo <66487319+isaborejo@users.noreply.github.com>
2020-07-05 23:50:04 -0300

    imobiliaria
    contém erros que serão resolvidos

---
+++ ImobiliariaApp/app/src/main/java/com/example/imobiliariaapp/MainActivity.java
@@ -0,0 +1,14 @@
+ jpackage com.example.imobiliariaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
